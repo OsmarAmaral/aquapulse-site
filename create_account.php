1a21d2b2c62e9baf10a00d36bedae8c4fc6f5665
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2025-01-07 14:38:11 -0400

    rename css-create-account.css => create_account.css

--- create_account.php
+++ create_account.php
@@ -60,7 +60,7 @@ $conn->close();
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/css/css-create-account.css">
+    <link rel="stylesheet" href="src/css/create_account.css">
     <script src="src/js/usuario.js"></script>
     <title>AquaPulse Criar Conta</title>
 </head>
